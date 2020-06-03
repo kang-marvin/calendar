9566d89def893b9fa314177efde9e64f03f88b77
Marvin Kang'ethe <marvin.kangethe@andela.com>
2020-06-03 19:39:43 +0300

    Add additional testing of how Stimulusreflex works.
     - (Note we haven't used Stimulus JS Controller)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @hello ||= 'Welcome {UserName}'
+    @words ||= ''
   end
 end
--- app/reflexes/welcome_reflex.rb
+++ app/reflexes/welcome_reflex.rb
@@ -4,4 +4,8 @@ class WelcomeReflex < ApplicationReflex
   def greetings
     @hello = "Welcome Marvin"
   end
+
+  def word
+    @words = element[:value]
+  end
 end
