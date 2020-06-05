3756bfe623ad93992027da25b6ec3c785efe90c9
Marvin Kang'ethe <marvinjameskang@gmail.com>
2020-06-06 02:08:47 +0300

    Design the calendar UI
     - today's date and time display
    
     - buttons for changing the month (back and forth)
    
     - calender days of the month table

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,22 @@
 class CalendarController < ApplicationController
+  DAYS_OF_THE_WEEK = [
+    'Sun', 'Mon', 'Tue',
+    'Wed', 'Thr', 'Fri', 'Sat'
+  ]
+
   def index
+    @dateObject = {
+      day: 'Wednesday',
+      date: 3,
+      month: 'June',
+      year: 2020
+    }
+
+    @timeObject = {
+      currentTime: '3:43:43',
+      meridiem: 'AM',
+    }
+
+    @days_of_the_week = DAYS_OF_THE_WEEK
   end
 end
