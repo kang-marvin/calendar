d2a28034fdbf5bae630c352cc19a3e77dcd07b27
Marvin Kang'ethe <marvinjameskang@gmail.com>
2020-06-07 03:36:27 +0300

    Generate sample code for the calendar days including last and next nth month days

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,15 +1,27 @@
 class CalendarController < ApplicationController
-  DAYS_OF_THE_WEEK = [
-    'Sun', 'Mon', 'Tue',
-    'Wed', 'Thr', 'Fri', 'Sat'
-  ]
+
+  TODAY = Date.today
+
+  DAYS_OF_THE_WEEK_OBJECT = {
+    'Sun': 0, 'Mon': 1, 'Tue': 2,
+    'Wed': 3, 'Thr': 4, 'Fri': 5,
+    'Sat': 6
+  }
+
+  NO_OF_WEEKS_TO_DISPLAY = 6
+
+  DAYS_OF_THE_MONTH = [
+    (27..31).to_a,
+    (1..30).to_a,
+    (1..9).to_a,
+].flatten[0...(DAYS_OF_THE_WEEK_OBJECT.count * NO_OF_WEEKS_TO_DISPLAY)]
 
   def index
     @dateObject = {
-      day: 'Wednesday',
-      date: 3,
-      month: 'June',
-      year: 2020
+      day:    I18n.t('date.day_names')[TODAY.wday],
+      date:   TODAY.day,
+      month:  I18n.t('date.month_names')[TODAY.month],
+      year:   TODAY.year
     }
 
     @timeObject = {
@@ -17,6 +29,8 @@ class CalendarController < ApplicationController
       meridiem: 'AM',
     }
 
-    @days_of_the_week = DAYS_OF_THE_WEEK
+    @days_of_the_week = DAYS_OF_THE_WEEK_OBJECT.keys
+    @days_of_the_month = DAYS_OF_THE_MONTH
+
   end
 end
